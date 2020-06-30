853a1c746770e7b01a49bf93999d6541f73e1fa3
afs <afs.gmail.com>
2020-06-30 11:44:38 +0530

    updated file

--- af_farhan/program.py
+++ af_farhan/program.py
@@ -2,4 +2,4 @@
 a = int(input("Enter a no.: "))
 b = int(input("Enter a no.: "))
 rem = a//b
-print(rem)
+print("Rem of a and b is :"rem)
--- far_af/program.py
+++
@@ -1,6 +0,0 @@
-#addition program
-a = int(input("Enter a no.: "))
-b = int(input("Enter a no.: "))
-sum = a+b
-pro = a*b
-print(sum)
